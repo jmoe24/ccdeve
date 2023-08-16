07779928ae1734d9c52fe3aac47b9803ea55c6a4
Expert <expert@ppm.example.com>
2023-08-16 22:30:32 +0000

    getting started with terraform

---
+++ 2_infrastructure-as-code/terraform/getting-started/variables.tf
@@ -0,0 +1,9 @@
+variable "user" {
+  description = "Login information"
+  type        = map
+  default     = {
+    username = "admin"
+    password = "1234QWer!"
+    url      = "https://192.168.86.240"
+  }
+}
