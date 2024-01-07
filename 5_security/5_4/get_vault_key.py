261f90f72e6463c8d92a867a536f4a02f511855a
Expert <expert@ppm.example.com>
2024-01-07 20:37:45 +0000

    working python job to retrieve secrets from vault with cert+token

--- 5_security/5_4/get_vault_key.py
+++ 5_security/5_4/get_vault_key.py
@@ -4,7 +4,6 @@
 Author: Josh Morris
 Description: Get secret from Vault Server
 '''
-
 import requests
 
 # Replace with your Vault server URL and token
@@ -12,22 +11,34 @@ VAULT_URL = 'https://vault.joshlab.com:8200'
 VAULT_TOKEN = 'hvs.lqL7JialYYuk7AVcCbHSr2cP'
 
 # Vault API endpoint to retrieve the secret
-SECRET_PATH = '/v1/cubbyhole/test'
+SECRET_NAME = 'test1'
+SECRET_PATH = f'/v1/cubbyhole/{SECRET_NAME}'
+
+# Path to your client certificate and private key files
+# Note: This path is hardcoded for the CWS
+CA_CERT_PATH = '/home/expert/gitrepo/4_containers/docker/vault_docker-compose/config/certs/vault_ca_certificate.crt'
+CLIENT_CERT_PATH = '/home/expert/gitrepo/4_containers/docker/vault_docker-compose/config/certs/vault_certificate.crt'
+CLIENT_KEY_PATH = '/home/expert/gitrepo/4_containers/docker/vault_docker-compose/config/certs/vault_privatekey.key'
 
 # Make the API request to Vault
 HEADERS = {'X-Vault-Token': VAULT_TOKEN}
-RESPONSE = requests.get(f'{VAULT_URL}{SECRET_PATH}', headers=HEADERS)
+response = requests.get(
+    f'{VAULT_URL}{SECRET_PATH}',
+    headers=HEADERS,
+    cert=(CLIENT_CERT_PATH, CLIENT_KEY_PATH),  # Provide client certificate and key
+    verify=CA_CERT_PATH
+)
 
 # Check if the request was successful (status code 200)
+
 if response.status_code == 200:
     # Extract the secret data from the response
     secret_data = response.json().get('data', {})
-    josh_test = secret_data.get('test', 'default_value')  # Replace 'key_name' with your specific field name
+    retrieved_password = secret_data.get(SECRET_NAME, 'default_value')
 
     # Print the retrieved secret value
-    print(f"Retrieved 'key_name' from the 'test' secret: {josh_test}")
+    print(f"Retrieved 'key_name' from the '{SECRET_NAME}' secret: {retrieved_password}")
 
-    # Now 'josh_test' holds the secret value retrieved from Vault
+    # Now 'retrieved_password' holds the secret value retrieved from Vault
 else:
     print(f"Failed to retrieve secret. Status code: {response.status_code}")
-
